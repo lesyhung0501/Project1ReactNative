ab8a9a57b6982eddb8e9bd90f80fc9abdb56d125
lesyhung0501 <91235849+lesyhung0501@users.noreply.github.com>
2022-04-11 15:25:02 +0700

    project1ReactNative

--- App.js
+++ App.js
@@ -1,10 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import {useState, useRef, useEffect} from 'react';
 
 export default function App() {
+  let [count, setCount] = useState(300)
+  let minius = Math.floor(count/60);
+  let seconds = count - minius * 60;
+  let timerId= useRef()
+
+  function handleStart() {
+    timerId.current = setInterval(() => {
+      setCount(prev => prev - 1)
+    }, 1000)
+  }
+
+  function handlePause() {
+    clearInterval(timerId.current)
+  }
+
+  function handleReset() {
+    clearInterval(timerId.current);
+    setCount(300);
+  }
+
+  useEffect(() => {
+    if(count === 0) {
+      clearInterval(timerId.current)
+    }
+    
+  }, [count])
+
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        {seconds <10? <Text>0{minius}:0{seconds}</Text>: <Text>0{minius}:{seconds}</Text> }
+        
+      </View>
+
+      <View style={styles.button}>
+        <Button
+          title='Start'
+          onPress={handleStart}
+        ></Button>
+      </View>
+
+      <View style={styles.button}>
+        <Button
+          title='Pause'
+          onPress={handlePause}
+        ></Button>
+      </View>
+
+      <View style={styles.button}>
+        <Button
+          title='Reset'
+          onPress={handleReset}
+        ></Button>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +69,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
   },
+  button: {
+    marginTop: 10
+  }
 });
